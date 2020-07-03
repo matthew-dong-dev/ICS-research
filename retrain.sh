bbe3e27c70660a9d479fa86422fea98e475a851a
Matthew Dong <mdong@berkeley.edu>
2020-07-03 14:02:51 -0700

    move parameters from bash script to python script

--- retrain.sh
+++ retrain.sh
@@ -1,47 +1,38 @@
 #!/bin/bash
 
-set -e 
+echo '[INFO] Beginning ICS retraining...'
+python ./scripts/data_joining.py
+echo '[INFO] Running semantic model...'
+python ./scripts/semantic_model.py -v ./data/aligned_course_vecs.tsv -i ./data/aligned_course_info.tsv
 
-while getopts ":b:v:" opt; do
-  case $opt in
-    b) 
-		export tfbias=$OPTARG
-		echo "[INFO] setting -b tfbias: $tfbias"
-      ;;
-    v) 
-		export vectorfile=$OPTARG
-		echo "[INFO] setting -v vectorfile: $vectorfile"
-      ;;
-    \?) 
-		echo "Usage: cmd [-b] (tf-bias) [-v] (vector file)"
-		echo "Invalid option: -$OPTARG" 
-		exit 1
-      ;;
-     :)
-	    echo "Option -$OPTARG requires an argument." 
-	    exit 1
-      ;;
-  esac
-done
+# set -e 
 
-# if [ $OPTIND -eq 1 ]; then echo "Exiting: No options were passed"; exit 1; fi
+# while getopts ":b:v:" opt; do
+#   case $opt in
+#     b) 
+# 		export tfbias=$OPTARG
+# 		echo "[INFO] setting -b tfbias: $tfbias"
+#       ;;
+#     v) 
+# 		export vectorfile=$OPTARG
+# 		echo "[INFO] setting -v vectorfile: $vectorfile"
+#       ;;
+#     \?) 
+# 		echo "Usage: cmd [-b] (tf-bias) [-v] (vector file)"
+# 		echo "Invalid option: -$OPTARG" 
+# 		exit 1
+#       ;;
+#      :)
+# 	    echo "Option -$OPTARG requires an argument." 
+# 	    exit 1
+#       ;;
+#   esac
+# done
 
-# ==================================================================
+# tf_bias_list=($(seq 0 .5 $tfbias))
+# for i in ${tf_bias_list[@]}; do
 
-tf_bias_list=($(seq 0 .5 $tfbias))
-# run Python script on range of tf-bias values
-for i in ${tf_bias_list[@]}; do
+#     printf "=========================== Training model with tf-bias: %s\n" "${i}"
+#     python ./scripts/semantic_model.py -v ./data/course_vecs.tsv -r ./data/course_info.tsv -t course_description -b ${i} 
 
-    printf "=========================== Training with tf-bias: %s\n" "${i}"
-    python ./scripts/semantic_model.py -v ./data/course_vecs.tsv -r ./data/course_info.tsv -t course_description -b ${i} 
-
-done
-
-echo "[INFO] Finished training keywords, proceed to group and get unique keywords for each course"
-
-python ./scripts/group_keywords.py
-
-# python ./scripts/data_joining.py
-# python ./scripts/semantic_model.py -v ./data/course_vecs.tsv -r ./data/course_info.tsv -t course_description -b $tfbias
-# python ./scripts/group_keywords.py
-
+# done
