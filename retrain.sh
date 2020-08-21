74f7a8dd28d2ce02a03778609dd756cc9a569a25
Matthew <mdong@berkeley.edu>
2020-08-20 17:38:48 -0700

    clean bash script

--- retrain.sh
+++ retrain.sh
@@ -1,33 +1,11 @@
 #!/bin/bash
 
+set -e 
+
 echo '[INFO] Beginning ICS retraining...'
 python ./scripts/data_joining.py
 echo '[INFO] Running semantic model...'
 python ./scripts/semantic_model.py -v ./data/aligned_course_vecs.tsv -i ./data/aligned_course_info.tsv
-
-# set -e 
-
-# while getopts ":b:v:" opt; do
-#   case $opt in
-#     b) 
-# 		export tfbias=$OPTARG
-# 		echo "[INFO] setting -b tfbias: $tfbias"
-#       ;;
-#     v) 
-# 		export vectorfile=$OPTARG
-# 		echo "[INFO] setting -v vectorfile: $vectorfile"
-#       ;;
-#     \?) 
-# 		echo "Usage: cmd [-b] (tf-bias) [-v] (vector file)"
-# 		echo "Invalid option: -$OPTARG" 
-# 		exit 1
-#       ;;
-#      :)
-# 	    echo "Option -$OPTARG requires an argument." 
-# 	    exit 1
-#       ;;
-#   esac
-# done
 
 # tf_bias_list=($(seq 0 .5 $tfbias))
 # for i in ${tf_bias_list[@]}; do
